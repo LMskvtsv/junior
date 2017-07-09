3e95318a92bd1fe54a7bae47988d4e70c0526afd
LMskvtsv <leedok@inbox.ru>
2017-07-09 23:52:47 +0300

    1.3 JavaDoc. Code convention[#18955]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -2,8 +2,17 @@ package ru.job4j;
 
 import java.io.*;
 
+/**
+  *Класс Calculate. Решение задачи части 1.3 JavaDoc. Code convention.
+  *@author Moskovtseva
+  *@sinse 09.07.2017
+*/
 public class Calculate {
 
+/**
+  *Вывод строки в консоль
+  *@param arg - arg.
+*/
 public static void main (String args[]){
 	System.out.println("Hello, World!");
   }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+  * Package for calculate task.
+  *
+  * @author Lidiya Moskovtseva (mailto:leedok@inbox.ru)
+  * @version $Id$
+  * @since 0.1
+*/
+package ru.job4j;
