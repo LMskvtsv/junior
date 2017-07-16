4dd02eba73d55f4e5f00f11e0730a700a3144dc1
LMskvtsv <leedok@inbox.ru>
2017-07-16 18:23:56 +0300

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,34 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Test Calculate.java
+ * 
+ * @author Lidiya Moskovtseva
+ * @version 1
+ * @sinse 2017-07-16
+ */
+
+public class CalculateTest {
+
+/**
+ * Test add.
+ */
+
+
+  @Test
+  public void whenAddOneToOneThanTwo(){
+
+  ByteArrayOutputStream out = new ByteArrayOutputStream();
+  System.setOut(new PrintStream(out));
+  Calculate.main(null);
+  assertThat(out.toString(), is(String.format("Hello, World!%s", System.getProperty("line.separator"))));
+
+  }
+}
