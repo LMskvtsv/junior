773531ccfedd12fd6d97e38ee693dae443c487e6
LMskvtsv <leedok@inbox.ru>
2017-10-29 22:38:23 +0200

    5.2. Создание программы поворота квадратного массива. [#29465]

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -12,13 +12,18 @@ public class BubbleSort {
      */
     public int[] sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
+            boolean isChanged = false;
             for (int j = 0; j < array.length - 1; j++) {
                 int tmp;
                 if (array[j] > array[j + 1]) {
                     tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
+                    isChanged = true;
                 }
+            }
+            if (!isChanged) {
+                break;
             }
         }
         return array;
---
+++ chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class for RotateArray test.
+ */
+public class RotateArrayTest {
+    /**
+     * Check if array will be rotated 2x2.
+     */
+    @Test
+    public void twoXTwo() {
+        int[][] a = new int[][]{{1, 2}, {3, 4}};
+        RotateArray ra = new RotateArray();
+        int[][] actual = ra.sort(a);
+        int[][] expected = new int[][]{{3, 1}, {4, 2}};
+        assertThat(actual, is(expected));
+    }
+
+    /**
+     * Check if array will be rotated 2x2.
+     */
+    @Test
+    public void threeXThree() {
+        int[][] a = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        RotateArray ra = new RotateArray();
+        int[][] actual = ra.sort(a);
+        int[][] expected = new int[][]{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+        assertThat(actual, is(expected));
+    }
+}
