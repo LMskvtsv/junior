b58ea2d582ce57657a736a75ce89223e0e3afae8
LMskvtsv <leedok@inbox.ru>
2018-04-21 22:01:14 +0300

    3. Компаратор для строк.[#50250] - review changes

--- chapter_003/src/main/java/comparator/ListCompare.java
+++ chapter_003/src/main/java/comparator/ListCompare.java
@@ -12,22 +12,15 @@ public class ListCompare implements Comparator<String> {
         int counter = firsts.length < second.length ? firsts.length : second.length;
 
         for (int i = 0; i < counter; i++) {
-            if (Character.compare(firsts[i], second[i]) > 0) {
-                result = 1;
+            result = Character.compare(firsts[i], second[i]);
+            if (result != 0) {
                 break;
-            } else if (Character.compare(firsts[i], second[i]) < 0) {
-                result = -1;
-                break;
-            } else if (Character.compare(firsts[i], second[i]) == 0) {
-                if (firsts.length > second.length) {
-                    result = 1;
-                } else if (firsts.length < second.length) {
-                    result = -1;
-                } else if (firsts.length == second.length) {
-                    result = 0;
-                }
             }
         }
+        if (result == 0 && firsts.length != second.length) {
+            result = Integer.compare(firsts.length, second.length);
+        }
+
         return result;
     }
 }
