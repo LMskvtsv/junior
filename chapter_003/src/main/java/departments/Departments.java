63b9504305c95e876f8fee273482c435b3fca5b3
LMskvtsv <leedok@inbox.ru>
2018-04-30 14:14:15 +0300

    Отсортировать департаменты[#51547]

--- chapter_003/src/main/java/departments/Departments.java
+++ chapter_003/src/main/java/departments/Departments.java
@@ -1,4 +1,41 @@
 package departments;
 
-public class SortDepartments {
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class Departments {
+
+    private TreeSet<String> departments = new TreeSet<>();
+
+    /**
+     * Adds distinct department into departments collection in Natural Order.
+     *
+     * @param values - array of department paths. Department path can be represented like: "K1\SK1\SSK1", split with '\'
+     */
+    public void addInNaturalOrder(String[] values) {
+        for (String s : values) {
+            departments.add(s);
+            while (s.lastIndexOf("\\") > -1) {
+                int i = s.lastIndexOf("\\");
+                s = s.substring(0, i);
+                departments.add(s);
+            }
+        }
+    }
+
+    public TreeSet<String> getDepartments() {
+        return departments;
+    }
+
+    /**
+     * Creates new object with Comparator required.
+     *
+     * @param departments which need to be sorted.
+     * @return - sorted departments.
+     */
+    public TreeSet<String> sort(TreeSet<String> departments, Comparator<String> comparator) {
+        TreeSet<String> newest = new TreeSet<>(comparator);
+        newest.addAll(departments);
+        return newest;
+    }
 }
--- chapter_003/src/test/java/departments/DepartmentsTest.java
+++ chapter_003/src/test/java/departments/DepartmentsTest.java
@@ -1,4 +1,38 @@
 package departments;
 
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class DepartmentsTest {
+    @Test
+    public void whenAddThanNaturalOrder() {
+        Departments departments = new Departments();
+        String[] values = new String[]{"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+        departments.addInNaturalOrder(values);
+        String[] expected = new String[]{"K1", "K1\\SK1", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K1\\SK2", "K2", "K2\\SK1", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+        assertThat(departments.getDepartments().toArray(), is(expected));
+    }
+
+    @Test
+    public void sortBackwards() {
+        Departments departments = new Departments();
+        String[] values = new String[]{"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+        departments.addInNaturalOrder(values);
+        String[] expected = new String[]{"K2\\SK1\\SSK2", "K2\\SK1\\SSK1", "K2\\SK1", "K2", "K1\\SK2", "K1\\SK1\\SSK2", "K1\\SK1\\SSK1", "K1\\SK1", "K1"};
+        assertThat(departments.sort(departments.getDepartments(), Comparator.reverseOrder()).toArray(), is(expected));
+    }
+
+    @Test
+    public void sortNaturalOrder() {
+        Departments departments = new Departments();
+        String[] values = new String[]{"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+        departments.addInNaturalOrder(values);
+        String[] expected = new String[]{"K1", "K1\\SK1", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K1\\SK2", "K2", "K2\\SK1", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+        departments.sort(departments.getDepartments(), Comparator.reverseOrder());
+        assertThat(departments.sort(departments.getDepartments(), Comparator.naturalOrder()).toArray(), is(expected));
+    }
 }
