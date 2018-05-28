3a548b7bdead2f7265aef5369e74560d36524d32
LMskvtsv <leedok@inbox.ru>
2018-05-28 16:15:29 +0300

    5.3.1. Создать динамический список на базе массива.[#54627] - review changes

--- chapter_005/src/test/java/list/DynamicArrayTest.java
+++ chapter_005/src/test/java/list/DynamicArrayTest.java
@@ -3,6 +3,8 @@ package list;
 import org.junit.Test;
 
 import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -28,5 +30,51 @@ public class DynamicArrayTest {
         for (int i : dynamicArray) {
             dynamicArray.add(14);
         }
+    }
+
+    @Test
+    public void whenHasNextThanHasNextTrue() {
+        DynamicArray<Integer> dynamicArray = new DynamicArray<>();
+        dynamicArray.add(11);
+        assertThat(dynamicArray.iterator().hasNext(), is(true));
+    }
+
+    @Test
+    public void whenHasntNextThanHasNextFalse() {
+        DynamicArray<Integer> dynamicArray = new DynamicArray<>();
+        dynamicArray.add(11);
+        dynamicArray.add(12);
+        dynamicArray.add(13);
+        Iterator i = dynamicArray.iterator();
+        i.next();
+        i.next();
+        i.next();
+        assertThat(i.hasNext(), is(false));
+    }
+
+    @Test
+    public void whenMultipleHasNextThanNextIsFirstElement() {
+        DynamicArray<Integer> dynamicArray = new DynamicArray<>();
+        dynamicArray.add(11);
+        dynamicArray.add(12);
+        dynamicArray.add(13);
+        Iterator i = dynamicArray.iterator();
+        i.hasNext();
+        i.hasNext();
+        i.hasNext();
+        assertThat(i.next(), is(11));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void checkEveryElement() {
+        DynamicArray<Integer> dynamicArray = new DynamicArray<>();
+        dynamicArray.add(11);
+        dynamicArray.add(12);
+        dynamicArray.add(13);
+        Iterator i = dynamicArray.iterator();
+        assertThat(i.next(), is(11));
+        assertThat(i.next(), is(12));
+        assertThat(i.next(), is(13));
+        i.next();
     }
 }
