d8bc30210cacd2e82e1d7236dc70dfb3a7e20a89
LMskvtsv <leedok@inbox.ru>
2018-05-30 13:59:48 +0300

    3. Переопределить только hashCode[#55362]

--- chapter_005/src/main/java/map/User.java
+++ chapter_005/src/main/java/map/User.java
@@ -1,6 +1,7 @@
 package map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
 
@@ -12,5 +13,10 @@ public class User {
         this.name = name;
         this.birthday = birthday;
         this.children = children;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthday, children);
     }
 }
--- chapter_005/src/test/java/map/UserTest.java
+++ chapter_005/src/test/java/map/UserTest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class UserTest {
     @Test
-    public void userMapDemonstration() {
+    public void userMapDemonstrationDifferentReferences() {
         Calendar calendar1 = Calendar.getInstance();
         calendar1.set(1990, Calendar.SEPTEMBER, 25);
         Calendar calendar2 = Calendar.getInstance();
@@ -19,6 +19,22 @@ public class UserTest {
         Map<User, Object> map = new HashMap<>();
         map.put(first, "first");
         map.put(second, "second");
+        System.out.println(map);
+        System.out.println(first.hashCode());
+        System.out.println(second.hashCode());
+    }
+
+    @Test
+    public void userMapDemonstrationSameReference() {
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.set(1990, Calendar.SEPTEMBER, 25);
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.set(1990, Calendar.SEPTEMBER, 25);
+        User first = new User("Ivan", calendar1, 0);
+        User second = new User("Ivan", calendar2, 0);
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, "first");
+        map.put(first, "second");
         System.out.println(map);
         System.out.println(first.hashCode());
         System.out.println(second.hashCode());
