6bea7a6746176c332d63239c047c4607a7b8d7d1
LMskvtsv <leedok@inbox.ru>
2018-06-12 10:50:53 +0300

    4. Поиск текста в файле.[#56650] - review changes

--- chapter_005/src/main/java/threads/ParallelSearch.java
+++ chapter_005/src/main/java/threads/ParallelSearch.java
@@ -1,8 +1,5 @@
 package threads;
 
-import net.jcip.annotations.GuardedBy;
-import net.jcip.annotations.ThreadSafe;
-
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -10,44 +7,39 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
-@ThreadSafe
 public class ParallelSearch extends SimpleFileVisitor<Path> {
 
     private final String root;
     private final String text;
-    private final List<String> exts;
-    final Object lock = new Object();
+    private final LinkedBlockingQueue<String> exts;
+
+    private final LinkedBlockingQueue<String> files = new LinkedBlockingQueue<>();
+
+    private final LinkedBlockingQueue<String> paths = new LinkedBlockingQueue<>();
 
 
-    volatile boolean finish = false;
-
-    @GuardedBy("this")
-    private final Queue<String> files = new LinkedList<>();
-
-    @GuardedBy("this")
-    private final List<String> paths = new ArrayList<>();
-
-
-    public ParallelSearch(String root, String text, List<String> exts) {
+    public ParallelSearch(String root, String text, LinkedBlockingQueue<String> exts) {
         this.root = root;
         this.text = text;
         this.exts = exts;
     }
 
     @Override
-    public FileVisitResult visitFile(Path filePath, BasicFileAttributes attrs) throws IOException {
+    public FileVisitResult visitFile(Path filePath, BasicFileAttributes attrs) {
         String[] split = filePath.toString().split("\\.");
         String ext = split[split.length - 1];
         for (String s : exts) {
             if (ext.equalsIgnoreCase(s)) {
-                paths.add(filePath.toString());
+                try {
+                    paths.offer(filePath.toString(), 1_000, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
         return CONTINUE;
@@ -57,45 +49,38 @@ public class ParallelSearch extends SimpleFileVisitor<Path> {
         Thread search = new Thread() {
             @Override
             public void run() {
-                synchronized (lock) {
-                    try {
-                        Files.walkFileTree(Paths.get(root), ParallelSearch.this);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    finish = true;
-                    lock.notifyAll();
+                try {
+                    Files.walkFileTree(Paths.get(root), ParallelSearch.this);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         };
         Thread read = new Thread() {
             @Override
             public void run() {
-
-                synchronized (lock) {
-                    if (!finish) {
-
+                try {
+                    String path;
+                    while ((path = paths.poll(1_000, TimeUnit.MILLISECONDS)) != null) {
+                        String content = null;
                         try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            return;
+                            content = new String(Files.readAllBytes(Paths.get(path)));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (content.toLowerCase().contains(text.toLowerCase())) {
+                            try {
+                                files.offer(path, 1_000, TimeUnit.MILLISECONDS);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
-                for (String s : paths) {
-                    String content = null;
-                    try {
-                        content = new String(Files.readAllBytes(Paths.get(s)));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (content.toLowerCase().contains(text.toLowerCase())) {
-                        files.offer(s);
-                    }
-                }
             }
-
         };
         search.start();
         read.start();
@@ -108,9 +93,8 @@ public class ParallelSearch extends SimpleFileVisitor<Path> {
         }
     }
 
-    synchronized Queue<String> result() {
+    synchronized LinkedBlockingQueue<String> result() {
         return this.files;
     }
-
 }
 
--- chapter_005/src/test/java/threads/ParallelSearchTest.java
+++ chapter_005/src/test/java/threads/ParallelSearchTest.java
@@ -2,14 +2,14 @@ package threads;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class ParallelSearchTest {
     @Test
     public void searchText() {
-        /*ArrayList<String> list = new ArrayList<>();
-        list.add("txt");
+        /*LinkedBlockingQueue<String> list = new LinkedBlockingQueue<>();
+        list.offer("txt");
         ParallelSearch search = new ParallelSearch("/Users/Leeda/Downloads/test", "bu", list);
         search.init();
         System.out.println(search.result());*/
