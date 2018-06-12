ad863f11a457ef03602e4995c0b56d86cc092cef
LMskvtsv <leedok@inbox.ru>
2018-06-12 10:27:12 +0300

    3. Реализовать собственный механизм блокировок Lock.[#56642] - review changes

--- chapter_005/src/main/java/threads/SimpleLock.java
+++ chapter_005/src/main/java/threads/SimpleLock.java
@@ -3,16 +3,22 @@ package threads;
 public class SimpleLock {
 
     private boolean isLocked = false;
+    private Thread currentThread;
 
     public synchronized void lock() throws InterruptedException {
         while (isLocked) {
             wait();
         }
         isLocked = true;
+        currentThread = Thread.currentThread();
     }
 
     public synchronized void unlock() {
-        isLocked = false;
-        notifyAll();
+        if (currentThread != null && Thread.currentThread().getId() == currentThread.getId()) {
+            isLocked = false;
+            notifyAll();
+        } else {
+            System.out.printf("Thread %s cannot unlock, because it was locked by thread %s", Thread.currentThread(), currentThread);
+        }
     }
 }
