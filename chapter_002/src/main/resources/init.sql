61f4b7519ed5cba6a5ba94865c80850f4bb45e92
LMskvtsv <leedok@inbox.ru>
2018-06-27 14:30:44 +0300

    small fixes

--- chapter_002/src/main/resources/init.sql
+++ chapter_002/src/main/resources/init.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS items
-(	  id integer primary key autoincrement,
+(	  forumID integer primary key autoincrement,
     name varchar(200),
     description varchar (200),
     created TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS comments
-(	  id integer primary key autoincrement,
+(	  forumID integer primary key autoincrement,
     comment varchar(500),
- 	  item_id integer references items(id),
+ 	  item_id integer references items(forumID),
     created TIMESTAMP
 );
