bd34ce64cd3b0f7f91278cf10dd836bb85970147
LMskvtsv <leedok@inbox.ru>
2018-07-26 11:59:06 +0300

    2. Лямбды и шаблон wrapper.[#63025]

--- toDoList/src/main/java/store/ItemsController.java
+++ toDoList/src/main/java/store/ItemsController.java
@@ -3,10 +3,11 @@ package store;
 import models.Item;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class ItemsController implements Controller<Item> {
 
@@ -14,31 +15,45 @@ public class ItemsController implements Controller<Item> {
 
     @Override
     public List<Item> getAll() {
-        List<Item> list = new ArrayList<>();
-        try (Session session = factory.openSession()) {
-            session.beginTransaction();
-            list.addAll(session.createQuery("from Item").list());
-            session.getTransaction().commit();
-        }
-        return list;
+        return transaction(session -> session.createQuery("from Item").list());
     }
 
     @Override
     public Item create(Item item) {
-        try (Session session = factory.openSession()) {
-            session.beginTransaction();
-            session.save(item);
-            session.getTransaction().commit();
-        }
+        transaction(session -> session.save(item));
         return item;
     }
 
     @Override
     public void update(Item entity) {
-        try (Session session = factory.openSession()) {
-            session.beginTransaction();
-            session.update(entity);
-            session.getTransaction().commit();
+      transactionVoid(session -> session.update(entity));
+    }
+
+    private <T> T transaction(final Function<Session, T> command) {
+        final Session session = factory.openSession();
+        final Transaction transaction = session.beginTransaction();
+        try {
+            return command.apply(session);
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            transaction.commit();
+            session.close();
+        }
+    }
+
+    private void transactionVoid(final Consumer<Session> command) {
+        final Session session = factory.openSession();
+        final Transaction transaction = session.beginTransaction();
+        try {
+            command.accept(session);
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            transaction.commit();
+            session.close();
         }
     }
 }
